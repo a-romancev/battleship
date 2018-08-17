c43c15ab8a0dd6898241583dfaff3b412160bae4
Pandalad1n <Xpandalad1nX@gmail.com>
2018-08-17 21:25:35 +0300

    Изменение размера поля\ кол-ва кораблей и понятности интерфейса

--- game.py
+++ game.py
@@ -1,12 +1,13 @@
 from board import Board, CellNotEmptyError
-from random import randint
 import os
 
 
 class Game:
-    def __init__(self):
-        self.my_board = Board(5, 3, show_ships=True)
-        self.target_board = Board(5, 3)
+    def __init__(self, size, ships):
+        self.my_board = Board(size, ships, show_ships=True)
+        self.target_board = Board(size, ships)
+        self.size = size
+        self.ships = ships
 
     def print_boards(self):
         os.system('clear')
@@ -19,21 +20,21 @@ class Game:
         self.print_boards()
         while True:
             try:
-                x, y = [int(c) for c in input("Guess x, y: ").split()]
+                x, y = [(int(c) - 1) for c in input("Guess row and column: ").split()]
                 self.target_board.shoot(x, y)
 
             except CellNotEmptyError:
-                print("You can't shoot same cords")
+                print("You can't shoot same cords.")
                 continue
 
             except (ValueError, IndexError):
-                print("Wrong x/y")
+                print("Wrong column or row.")
                 continue
 
             self.my_board.shoot_random()
             self.print_boards()
             if not self.my_board.has_alive:
-                print("You loose!FOOOL")
+                print("You loose!FOOOL.")
                 return
             if not self.target_board.has_alive:
                 print("You won!")
--- main.py
+++ main.py
@@ -1,6 +1,18 @@
 
 from game import Game
 
-Game().start()
+size = 0
+ships = 1
+
+while (size < ships) or (size < 1):
+    size = int(input("Size of the board?: "))
+    if size < 2:
+        print("You cant have a board that small.")
+    else:
+        ships = int(input("How many ships do you want?: "))
+        if ships > size:
+            print("Too many ships, you can place a maximum of ", size, ".")
+        else:
+            Game(size, ships).start()
 
 
